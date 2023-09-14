94db61a9ec1151fd083a3794cc10b1ca6d25878e
romeusorionaet <romeusoares14569@gmail.com>
2023-09-14 19:40:55 -0300

    fix

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,10 @@ app.register(uploadVideoRoute)
 
 app.listen({
     port: 3333,
+    host: '0.0.0.0',
 }).then(() => {
     console.log('HTTP Server Running! Port: 3333')
+})
+.catch((err) => {
+    console.log(err)
 })
