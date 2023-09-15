c2f4a959e421c0de98e96c2b90135c4037620a29
romeusorionaet <romeusoares14569@gmail.com>
2023-09-15 10:49:58 -0300

    fix path for prod

--- src/routes/upload-video.ts
+++ src/routes/upload-video.ts
@@ -24,26 +24,31 @@ export async function uploadVideoRoute(app: FastifyInstance) {
         }
 
         const extension = path.extname(data.filename)
-
+        
         if (extension !== '.mp3') {
             return reply.status(400).send({ error: 'Invalid input type, please upload a MP3.'})
         }
 
         const fileBaseName = path.basename(data.filename, extension)
         const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`
-        const uploadDestination = path.resolve(__dirname, '../../tmp', fileUploadName)
+        const uploadDestination = path.resolve(__dirname, '../tmp', fileUploadName)
 
-        await pump(data.file, fs.createWriteStream(uploadDestination))
+        try{
+            await pump(data.file, fs.createWriteStream(uploadDestination))
 
-        const video = await prisma.video.create({
-            data: {
-                name: data.filename,
-                path: uploadDestination,
+            const video = await prisma.video.create({
+                data: {
+                    name: data.filename,
+                    path: uploadDestination,
+                }
+            })
+            
+            return {
+                video,
             }
-        })
 
-        return {
-            video,
+        }catch(err) {
+            return err
         }
     })
 } 
